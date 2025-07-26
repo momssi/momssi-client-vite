5df4de31f53bfae18f8d323c2140ef870278e16e
kjuiop <copying_y@naver.com>
2025-07-26 09:46:42 +0900

    fix: asset 파일에 대해 상대경로로 변경

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/',
-})
+  base: './', // 상대 경로로 빌드
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  }
+}) 
