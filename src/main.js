bb8b7c6871a5c5055030f531ac6f877019c10b2c
kjuiop <copying_y@naver.com>
2025-08-05 01:03:19 +0900

    feat: 성경 아이콘 변경

--- src/main.js
+++ src/main.js
@@ -158,7 +158,7 @@ document.querySelector('#app').innerHTML = `
     <div class="fixed bottom-0 left-0 right-0 bg-brown-800 text-white">
       <div class="max-w-md lg:max-w-4xl mx-auto">
         <div class="flex justify-around py-2">
-          <div class="flex flex-col items-center py-2 px-3">
+          <div class="flex flex-col items-center py-2 px-3 bg-brown-700 rounded-lg">
             <svg class="w-6 h-6 mb-1" fill="currentColor" viewBox="0 0 20 20">
               <path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd"/>
             </svg>
@@ -171,8 +171,8 @@ document.querySelector('#app').innerHTML = `
             <span class="text-xs">질문하기</span>
           </div>
           <div class="flex flex-col items-center py-2 px-3">
-            <svg class="w-6 h-6 mb-1" fill="currentColor" viewBox="0 0 20 20">
-              <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+            <svg class="w-6 h-6 mb-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
             </svg>
             <span class="text-xs">성경</span>
           </div>
@@ -182,7 +182,7 @@ document.querySelector('#app').innerHTML = `
             </svg>
             <span class="text-xs">콘텐츠</span>
           </div>
-          <div class="flex flex-col items-center py-2 px-3 bg-brown-700 rounded-lg">
+          <div class="flex flex-col items-center py-2 px-3">
             <svg class="w-6 h-6 mb-1" fill="currentColor" viewBox="0 0 20 20">
               <path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z"/>
             </svg>
